75f1ea8eeb24dae3f18583c0af0d26fa680a3403
emil <emilquluzade31.05@gmail.com>
2024-04-30 23:29:42 +0400

    dad

--- src/App.jsx
+++ src/App.jsx
@@ -2,31 +2,33 @@ import { useState } from "react";
 
 import "./App.css";
 
-function App(){
-  const [isDark, setIsDark] = useState({
-    color: "black",
-    "background-color": "white",
-    border: " 2px solid  black",
-  });
+function App() {
+  const [isDark, setIsDark] = useState(false);
   const changeMode = () => {
-    if (document.querySelector(".wrapper").style.color == "black") {
-      setIsDark({
-        color: "yellow",
-        "background-color": "black",
-        border: " 2px solid  yellow",
-      });
-    } else {
-      setIsDark({
-        color: "black",
-        "background-color": "white",
-        border: " 2px solid  black",
-      });
-    }
+    setIsDark(!isDark);
   };
 
+const dark={
+  color: "yellow",
+  "background-color": "black",
+  "border-color": "  yellow",
+}
+const light={
+  color: "black",
+  "background-color": "white",
+  "border-color": " black",
+}
+
   return (
-    <div className="wrapper" style={isDark}>
-      <button className="lightBtn" onClick={changeMode} style={isDark}>
+    <div
+      className="wrapper"
+      style={isDark?dark:light}
+    >
+      <button
+        className="lightBtn"
+        onClick={changeMode}
+        style={isDark?dark:light}
+      >
         White Mode
       </button>
       <h1 className="title">WELCOME TO REACT JS</h1>
